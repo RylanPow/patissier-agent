bb05f616c8fcc97979e7787e4a83cea5f8e9b656
Rylan Pow <powrylan@gmail.com>
2026-09-09 07:20:24 -0700

    typo in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Patissier"
-    SYNC_DATABASE_URL: set
+    SYNC_DATABASE_URL: str
     DATABASE_URL : str #async URL for fastapi gateway later
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
